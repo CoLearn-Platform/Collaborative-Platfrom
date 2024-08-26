94d090b5a3371f3f5b76b056058eb2a2af906170
Mahipal-Singh01 <mahivis0123@gmail.com>
2024-08-26 13:18:05 +0530

    Only Main pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 function App() {
   return (
-    <div>
-      SIH project
-    </div>
+    <Fragment>
+        <Routes>
+          <Route path="/" element = {<Layout />}/>
+          <Route index element = {<Home />}/>
+          <Route path="/" element = {<Project/>}/>
+          <Route path="/" element = {<Learing_Room/>}/>
+          <Route path="/" element = {<About_Us/>}/>
+        </Routes>
+    </Fragment>
   )
 }
 
