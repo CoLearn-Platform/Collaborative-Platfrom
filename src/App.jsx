b5b7a6740b19123b4b62951efdedecf5be332086
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-27 10:48:04 +0530

    setting up react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import AppLayout from "./ui/AppLayout";
 import Projects from "./features/projects/Projects";
 import Home from "./ui/Home";
 import Rooms from "./features/rooms/Rooms";
 import About from "./ui/About";
 
+// creating react query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    staleTime: 1000 * 60 * 1, // 1 minutes
+  },
+});
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="home" />} />
-            <Route path="home" element={<Home />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="rooms" element={<Rooms />} />
-            <Route path="about" element={<About />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="home" />} />
+              <Route path="home" element={<Home />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="rooms" element={<Rooms />} />
+              <Route path="about" element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
