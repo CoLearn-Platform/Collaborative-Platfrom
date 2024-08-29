103ced7b0a000e01f5c7fd9f90041c0cdb5754d5
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-29 21:56:09 +0530

    setting up environment variable

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://bqdkeybeedynvjgvdosq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZGtleWJlZWR5bnZqZ3Zkb3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2MDIxOTYsImV4cCI6MjA0MDE3ODE5Nn0.vTDbgj7nklqbz1aj_9JFqafgIx9VBTTqQzFxYDesl8Y";
+const supabaseUrl = import.meta.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
